68ff8fbf56eb9c89543572038d20fe31c5c817b7
Aleksey <dovbnya@intaro.ru>
2018-02-28 19:37:12 +0300

    pstrnav

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,111 +1,106 @@
-import React, {PureComponent} from 'react';
-import {connect} from "react-redux";
-import { browserHistory } from 'react-router';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { browserHistory } from "react-router";
 
 //import { selectBtc, selectEth, selectOffset, fetchUserRequest } from "../../actions/currency";
-import { fetchPokemonListRequest, fetchPokemonListSuccess, fetchPokemonListFailure } from "../../actions/pokemonList";
+import {
+  fetchPokemonListRequest,
+  fetchPokemonListSuccess,
+  fetchPokemonListFailure
+} from "../../actions/pokemonList";
+
 import { setCurrentPage } from "../../actions/pageInfo";
 
-import Loader from 'react-svg-spinner';
-import PstrNav from '../PstrNav';
+import Loader from "react-svg-spinner";
+import PstrNav from "../PstrNav";
 
 import {
-    getPokemonList,
-    isFetchingPokemonList,
-    isErrorPokemonList
+  getPokemonList,
+  isFetchingPokemonList,
+  isErrorPokemonList
 } from "../../reducers/pokemonList";
 
 import {
-    getPokemonData,
-    isFetchingPokemonData,
-    isErrorPokemonData
+  getPokemonData,
+  isFetchingPokemonData,
+  isErrorPokemonData
 } from "../../reducers/pokemonData";
 
 import { getCurrentPage } from "../../reducers/pageInfo";
 
 import { PokemonTable } from "../PokemonTable/PokemonTable";
 //import Pagination from "react-js-pagination";
-import ReactPaginate from 'react-paginate';
+//import ReactPaginate from "react-paginate";
 
+export class App extends Component {
+  componentDidMount() {
+    console.log("component did mount");
+    const currentPage = this.props.match.params.id;
+    this.props.fetchPokemonListRequest(currentPage);
+  }
+  componentWillReceiveProps(nextProps) {
+    console.log("will receive props");
+    const currentPage = this.props.match.params.id;
+    const nexPage = nextProps.match.params.id;
 
-
-export class App extends PureComponent{
-
-    componentDidMount() {
-       console.log('component did mount');
-       const currentPage = this.props.match.params.id;
-       this.props.fetchPokemonListRequest(currentPage);
+    if (currentPage !== nexPage) {
+      this.props.fetchPokemonListRequest(nexPage);
     }
-    componentWillReceiveProps(nextProps) {
-        console.log('will receive props');
-        const currentPage = this.props.match.params.id;
-        const nexPage = nextProps.match.params.id;
+  }
 
-        if( currentPage !== nexPage){
-            this.props.fetchPokemonListRequest(nexPage);
-        }
+  /* shouldComponentUpdate() {
+    console.log(this.props.isFetchingPokemonList);
+    console.log(this.props.isFetchingPokemonData);
+    if (this.props.isFetchingPokemonList || this.props.isFetchingPokemonData) {
+      return false;
+    } else {
+      return true;
     }
+  } */
 
-    getPokemons = () => {
-    
-        const currentPage = this.props.match.params.id;
-        this.props.fetchPokemonListRequest(currentPage);
-    }
+  getPokemons = () => {
+    const currentPage = this.props.match.params.id;
+    this.props.fetchPokemonListRequest(currentPage);
+  };
 
-    handlePageClick = (a) => {
-        let page = a.selected + 1 || 1;
-        //this.props.history.push(`/page/${page}`);
-    }
+  render() {
+    console.log("============================");
+    console.log("APP RENDER");
+    const isFetching =
+      this.props.isFetchingPokemonList || this.props.isFetchingPokemonData;
 
-    render(){
-     
-        console.log("============================");
-        console.log("APP RENDER");
-        const isFetching = this.props.isFetchingPokemonList || this.props.isFetchingPokemonData;
+    let pokemons = this.props.pokemonData;
 
-        let pokemons = this.props.pokemonData;
-
-        return(
-            <div className="app">
-
-                <div>
-                    { isFetching ? 
-                        <Loader size="70px" gap={4} color="green" /> 
-                        :
-                        <div>
-                            <PokemonTable pokemons={pokemons} />
-                            <PstrNav page={this.props.match.params.id} />
-                            <ReactPaginate
-                                hrefBuilder={i=>`/page/${i}`}
-                                pageCount={41}
-                                marginPagesDisplayed={2}
-                                pageRangeDisplayed={5}
-                                initialPage={1}
-                                forcePage={this.props.match.params.id}
-                                onPageChange={this.handlePageClick}
-                            />
-                        </div>
-                    }
-                </div>
-
-           </div>
-                
-       
-        );
-    };
+    return (
+      <div className="app">
+        <div>
+          {isFetching ? (
+            <Loader size="70px" gap={4} color="green" />
+          ) : (
+            <div>
+              <PokemonTable pokemons={pokemons} />
+              <PstrNav page={this.props.match.params.id} />
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    pokemonList: getPokemonList(state),
-    isFetchingPokemonList: isFetchingPokemonList(state),
-    isErrorPokemonList: isErrorPokemonList(state),
-    pokemonData: getPokemonData(state),
-    isFetchingPokemonData: isFetchingPokemonData(state),
-    isErrorPokemonData: isErrorPokemonData(state),
-    currentPage: getCurrentPage(state)
+  pokemonList: getPokemonList(state),
+  isFetchingPokemonList: isFetchingPokemonList(state),
+  isErrorPokemonList: isErrorPokemonList(state),
+  pokemonData: getPokemonData(state),
+  isFetchingPokemonData: isFetchingPokemonData(state),
+  isErrorPokemonData: isErrorPokemonData(state),
+  currentPage: getCurrentPage(state)
 });
+
 const mapDispatchToProps = {
-    fetchPokemonListRequest,
-    setCurrentPage
+  fetchPokemonListRequest,
+  setCurrentPage
 };
+
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/PstrNav/PstrNav.js
+++ src/components/PstrNav/PstrNav.js
@@ -1,39 +1,67 @@
-import React, {PureComponent} from 'react';
-import {connect} from "react-redux";
-import {Link} from 'react-router-dom'
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 import "./PstrNav.css";
 
-import {getPokemonCount, getPokemonOnPage} from '../../reducers/pageInfo';
+import { getPokemonCount, getPokemonOnPage } from "../../reducers/pageInfo";
 
-export class PstrNav extends PureComponent{
+export class PstrNav extends PureComponent {
+  getPages = () => {
+    const pages = Math.ceil(this.props.pokemonCount / this.props.pokemonOnPage); // Количество страниц
+    const prev = 4; // Возможное количество элементов перед текущей страницей
+    const currentPage = parseInt(this.props.page, 10);
+    let pagesArr = [];
+    let delimeterFlag = false;
 
-    pageRender = (i) => {
-        return (
-            <Link to={`/page/${i}`} className="pstr-link" key={i}>{i}</Link>
-        );
-    }
-
-    render(){
-
-        let pages = Math.ceil( this.props.pokemonCount/this.props.pokemonOnPage ); 
-        let arr = [];
-        
-        for(let i = 1; i <= pages; i++){
-            arr.push(this.pageRender(i));
+    for (let i = 1; i <= pages; i++) {
+      if (
+        ((i < currentPage && currentPage - 1 > prev) ||
+          (i > currentPage && pages - currentPage > prev)) &&
+        ((i < currentPage - 2 && i > 2) ||
+          (i > currentPage + 2 && i < pages - 1))
+      ) {
+        if (!delimeterFlag) {
+          pagesArr = [...pagesArr, <span className="pstr-nav__link">...</span>];
+          delimeterFlag = true;
         }
+        continue;
+      }
 
-        return (
-            <div>
-                {arr}
-            </div>
-        );
+      pagesArr =
+        i === currentPage
+          ? [...pagesArr, this.pageRender(i, "pstr-nav__link_active")]
+          : [...pagesArr, this.pageRender(i)];
+      delimeterFlag = false;
     }
+
+    return pagesArr;
+  };
+
+  pageRender = (i, activeClass) => {
+    return (
+      <Link
+        to={`/page/${i}`}
+        className={
+          activeClass ? "pstr-nav__link " + activeClass : "pstr-nav__link "
+        }
+        key={i}
+      >
+        {i}
+      </Link>
+    );
+  };
+
+  render() {
+    const pages = this.getPages();
+
+    return <div className="pstr-nav">{pages}</div>;
+  }
 }
 
 const mapStateToProps = state => ({
-    pokemonCount: getPokemonCount(state),
-    pokemonOnPage: getPokemonOnPage(state)
+  pokemonCount: getPokemonCount(state),
+  pokemonOnPage: getPokemonOnPage(state)
 });
 
 export default connect(mapStateToProps, null)(PstrNav);
